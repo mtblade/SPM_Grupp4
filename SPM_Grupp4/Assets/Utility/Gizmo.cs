235de49150c99917f0aefec9dea356c12c9ce139
mtblade <robinsengkey@hotmail.com>
2018-04-18 16:31:53 +0200

    To_Chris
    Co-Authored-By: ChrisMacks <chrismacks@users.noreply.github.com>

---
+++ SPM_Grupp4/Assets/Utility/Gizmo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gizmo : MonoBehaviour {
+
+    public float Range;
+
+    private void Start()
+    {
+
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, Range);
+    }
+}
